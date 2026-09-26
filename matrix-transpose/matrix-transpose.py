d067ab99bf0140b99689abce40b4fc8b0bf0de00
CaptJackSparrow21 <110366509+CaptJackSparrow21@users.noreply.github.com>
2026-09-26 23:50:13 +0530

    Solve matrix-transpose

--- matrix-transpose/matrix-transpose.py
+++ matrix-transpose/matrix-transpose.py
@@ -3,6 +3,9 @@ import numpy as np
 def matrix_transpose(A) :
     """
     Return the transpose of matrix A (swap rows and columns)
+    
+    Direct method - 
+        return np.asarray(A).T
     """
 
     A = np.asarray(A)
